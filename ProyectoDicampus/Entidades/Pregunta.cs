1476af2764b50d0c9de31c41e80e2960c2fa1766
Sergio Acebal <sergio98.am30@gmail.com>
2020-01-30 08:54:06 +0100

    Creación entidades

---
+++ ProyectoDicampus/Entidades/Pregunta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDicampus.Entidades
+{
+    public class Pregunta
+    {
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public String Nombre { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public String Respuesta1 { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public String Respuesta2 { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public String Respuesta3 { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public String Respuesta4 { get; set; }
+        [Required]
+        public int Correcta { get; set; }
+
+        //Relación
+        public int UsuarioID { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ ProyectoDicampus/Entidades/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDicampus.Entidades
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public String Username { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public String Password { get; set; }
+        [Required]
+        public Boolean Genero { get; set; }
+        [Required]
+        [Range(0, 99999)]
+        public int Puntuacion { get; set; }
+    }
+}
